80c34539a5033f3c54844df7bfd4cea7d4f75900
Andreas Ottosson <andreas@mgns.se>
2022-06-02 07:09:05 +0000

    - Add Prettier for Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-2xl">React TypeScript Sandbox</h1>
+      <h1 className="p-6 text-center text-2xl">React TypeScript Sandbox</h1>
     </div>
   )
 }
