093ca47b41c5a3f5d6fd1dfb8a407b1f34d7783e
Andreas Ottosson <andreas@mgns.se>
2022-06-02 06:47:07 +0000

    Inital commit...

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-2xl">React TypeScript Sandbox</h1>
+    </div>
+  )
+}
+
+export default App
